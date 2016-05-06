1ecd9c6b50090c5e41bb1afb2e08a0559baae526
Jeff Mery <jmery@chef.io>
2016-05-06 12:13:15 -0500

    Added some basic testing

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,9 +1,34 @@
 require 'spec_helper'
 
 describe 'aar::default' do
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+
+  describe package('apache2') do
+    it { should be_installed }
+  end
+
+  describe service('apache2') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe port('80') do
+    it { should be_listening }
+  end
+
+  describe package('mysql-server') do
+    it { should be_installed }
+  end
+
+  describe service('mysql') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe port('3306') do
+    it { should be_listening }
+  end
+
+  describe command('curl localhost') do
+    its(:stdout) { should match /This is the log on page for Awesome Appliance/ }
   end
 end
